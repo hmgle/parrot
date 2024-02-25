48db508f895da6d7b179f8c04e5f2eb014f10025
mingang.he <dustgle@gmail.com>
2024-02-25 23:22:07 +0800

    Add token.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module parrot
+
+go 1.22.0
---
+++ internal/token/token.go
@@ -0,0 +1,134 @@
+package token
+
+type Type int
+
+type Token struct {
+	Type    Type
+	Literal string
+	Pos     int
+}
+
+const (
+	ERR    Type = iota
+	EOF         // "eof"
+	LPAR        // "("
+	RPAR        //  ")"
+	LBRK        // "["
+	RBRK        // "]"
+	LBRACE      // "{"
+	RBRACE      // "}"
+	IN          // "in"
+
+	OR   // or
+	AND  // and
+	BANG // "NOT"
+	LT   // "LT"
+	GT   // "GT"
+	LE   // "LE"
+	GE   // "GE"
+	EQ   // "EQ"
+	ASSIGN
+	NOTEQ // "NE"
+	IDENT
+	NUM // "number"
+	STR // "string"
+
+	LEN // "len"
+	REG // "regexp"
+
+	TRUE  // "true"
+	FALSE // "false"
+
+	DOT    // "."
+	DOTDOT // ".."
+
+	ADD   // "add"
+	MINUS // "minus"
+	MUL   // "mul"
+	DIV   // "div"
+	MOD   // "mod"
+
+	NEGSIGN
+	POSSIGN
+
+	COLON     // :
+	COMMA     // ,
+	SEMICOLON // ;
+
+	RANGE
+	INDEX
+
+	CntToken
+)
+
+var typeNames = [...]string{
+	EOF:    "eof",
+	LPAR:   "(",
+	RPAR:   ")",
+	LBRK:   "[",
+	RBRK:   "]",
+	LBRACE: "{",
+	RBRACE: "}",
+	IN:     "in",
+
+	OR:     "or",
+	AND:    "and",
+	BANG:   "NOT",
+	LT:     "<",
+	GT:     ">",
+	LE:     "<=",
+	GE:     ">=",
+	EQ:     "==",
+	ASSIGN: "=",
+	NOTEQ:  "!=",
+	IDENT:  "identifier",
+	NUM:    "number",
+	STR:    "string",
+
+	LEN: "len",
+	REG: "regexp",
+
+	TRUE:  "true",
+	FALSE: "false",
+
+	DOT:    ".",
+	DOTDOT: "..",
+
+	ADD:   "add",
+	MINUS: "minus",
+	MUL:   "mul",
+	DIV:   "div",
+	MOD:   "mod",
+
+	NEGSIGN: "-",
+	POSSIGN: "+",
+
+	COLON:     ":",
+	COMMA:     ",",
+	SEMICOLON: ";",
+
+	RANGE: "range",
+	INDEX: "index",
+}
+
+func (t Type) String() string {
+	return typeNames[t]
+}
+
+var keywords = map[string]Type{
+	"and": AND,
+	"or":  OR,
+
+	"true":  TRUE,
+	"false": FALSE,
+
+	"in":     IN,
+	"regexp": REG,
+}
+
+func LookupKeyWord(identifier string) Type {
+	if tok, ok := keywords[identifier]; ok {
+		return tok
+	}
+	return IDENT
+}
